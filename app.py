ae0cbc4574298dc700854c123a5007267a033e2c
Brayan Pereira <brayan@codewithus.com>
2026-03-25 10:04:28 +1100

    final message edit

--- app.py
+++ app.py
@@ -42,26 +42,17 @@ Clues = {
                     one with nature is important. For this challenge, 
                     take a photo with the view!
                     ''',
-    "peace-clue":'''
-                    Happy Birthday, Anushe! I hope you liked your adventure. It took time and effort, but it's nothing compared to what you provide as a friend.
-                    As you know, I am always second guessing myself, mostly because my 
-                    brain has a habit of sabotaging me. But honestly, around you,
-                    my brain shuts off, and I feel so content. Then sometime during our time together, 
-                    after the 100th time you've made me laugh, everything is perfect enough for my brain to turn
-                    back on to make sure I know how lucky I am to know you. You are an amazing person, Anushe.
-                    You are smart, silly and serious all at the same time. Sometimes when you are garnering all the attention in a room by just being yourself, I have this feeling that brings so much joy and nostalgia for a time where everything was simple. I don't say this often, and to be honest I wish I could. I care about 
-                    you immensely, Anushe. I'm not talking about a sort of care that dulls over time. 
-                    It is my deepest hope you realize you will never be truly 
-                    alone. That when you feel lost or unsure who to 
-                    lean on, that you, without hesitation, will feel like you can think of me. 
-                    Forget all the thoughts that invade your head, telling you it's too 
-                    much to ask, I will always, enthusiastically, without any second thought, 
-                    be there for you. But I'm not special, I hope you know 
-                    that all people that call you a friend feel the same way. I know your life 
-                    will be full of adventure, wonder, and love. This was my attempt to give you a little 
-                    taste of that today.
-                    With much love, Brayan
-    ''',
+    "peace-clue":'''Happy Birthday, Anushe! 
+
+I hope you liked your adventure. It took time and effort, but it's nothing compared to what you provide as a friend. As you know, I am always second guessing myself, mostly because my brain has a habit of sabotaging me. But honestly, around you, my brain shuts off, and I feel so content in being myself. But my favorite part our time together comes later in the night, after the 100th time you've made me laugh, when my brain suddenly turns back on to make sure I know how lucky I am to know you. You are an amazing person, Anushe. You are smart, silly and serious all at the same time. Sometimes when you are garnering all the attention in a room by just being yourself, I have this feeling that brings so much joy and nostalgia for a time where everything was simple. 
+
+I don't say this often, and to be honest I wish I could. I care about you immensely, Anushe. I'm not talking about a sort of care that dulls over time. It is my deepest hope you realize you will never be truly alone. That when you feel lost or unsure who to lean on, that you, without hesitation, will feel like you can think of me. Forget all the thoughts that invade your head, telling you it's too much to ask, I will always, enthusiastically, without any second thought, be there for you. But I'm not special, I hope you know that all people that call you a friend feel the same way. 
+
+I know your life will be full of adventure, wonder, and love. This was my humble and inspired attempt to give you a little taste of that today.
+
+With much love, 
+
+Brayan''',
     "paint-clue":'''Pretty cool huh? It challenges our perception of reality
                     and makes us pay attention. For this challenge, you will solve the
                     Anushe themed crossword! Once done take a picture and submit it on the next page.''',
@@ -139,7 +130,12 @@ def letter(path):
         abort(404)
     
     clue_img_url = f'/static/clue-imgs/{path}.jpg'
-    cleaned_text = clean_text(Clues[path])
+    
+    if(path == 'peace-clue'):
+        clean_text = Clues[path]
+    else:
+        cleaned_text = clean_text(Clues[path])
+
     pages = [[clue_img_url, cleaned_text]]
 
     return render_template('blue-letter.html', 
